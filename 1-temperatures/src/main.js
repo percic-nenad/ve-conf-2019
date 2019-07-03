21c492cdc65421f49ce1d8ec23f6c44333fe4eba
Ruben Kovacevic <kovacevic.ruben@gmail.com>
2019-07-03 15:01:09 +0200

    Deleted remaining unnecessary stuff.

--- 1-temperatures/src/main.js
+++
@@ -1,7 +0,0 @@
-function sortAbsSpecial(a, b) {
-  return Math.abs(a) - Math.abs(b) || b - a;
-}
-
-export default function temperatures(input) {
-  return [...input].sort(sortAbsSpecial)[0] || 0;
-}
--- 3-chuck-norris/src/main.js
+++
@@ -1,5 +0,0 @@
-// Submit a pull request with a solution to github percic-nenad/ve-conf-2019
-
-export default function chuckNorris(input) {
-  return "Chuck Norris";
-}

ebf52d472064ee845b6069f07dfb6d6d99d2d2ba
Kosta Kupresak <kupresak98@gmail.com>
2019-07-03 15:51:51 +0200

    Added regex

--- 3-chuck-norris/src/main.js
+++ 3-chuck-norris/src/main.js
@@ -1,43 +1,19 @@
 // Submit a pull request with a solution to github percic-nenad/ve-conf-2019
 
-function ascii (a) { 
-  var retVal = a.charCodeAt(0).toString(2);
+function toBinnary(x) { 
+  var retVal = x.charCodeAt(0).toString(2);
   while(retVal.length < 7)
     retVal = '0' + retVal
   return retVal;
 }
 
-export default function chuckNorris(input) {
-  var cloneArray = Array.from([...input].map(ascii).join("")); 
-  var last = -1;
-  var retVal = '';
-  cloneArray.forEach(function(x) {
-    if (last == -1) {
-      if (x == 1) {
-        retVal += '0 0';
-        last = 1;
-      } else if (x == 0) {
-        retVal += '00 0';
-        last = 0;
-      }
-    } else if (last == 0) {
-      if (x == 1) {
-        retVal += ' 0 0';
-        last = 1;
-      } else if (x == 0) {
-        retVal += '0';
-        last = 0;
-      }
-    } else if (last == 1) {
-      if (x == 1) {
-        retVal += '0';
-        last = 1;
-      } else if (x == 0) {
-        retVal += ' 00 0';
-        last = 0;
-      }
-    }
-  });
+function replaceZerosAndOnes(x) {
+  return x
+          .replace(/(0+)/, " 00 " + "0".repeat(x.length))
+          .replace(/(1+)/, " 0 " + "0".repeat(x.length));
+}
 
-  return retVal;
+export default function chuckNorris(input) {
+  var cloneArray = [...input].map(toBinnary).join("").split(/(1+)/); 
+  return cloneArray.map(replaceZerosAndOnes).join("").substr(1);
 } 
